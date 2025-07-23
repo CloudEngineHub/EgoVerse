83cb7f39eedf7ae666d233a9b624e587bc6a5cd2
ryanthecreator <ryanpunamiya@gmail.com>
2025-07-23 16:36:01 -0400

    current WIP

--- egomimic/scripts/evaluation/test2.py
+++ egomimic/scripts/evaluation/test2.py
@@ -13,18 +13,22 @@ import torchvision
 
 from rldb.utils import *
 
-from interbotix_common_modules.common_robot.robot import (
-    create_interbotix_global_node,
-    robot_shutdown,
-    robot_startup,
-)
+# from interbotix_common_modules.common_robot.robot import (
+#     create_interbotix_global_node,
+#     robot_shutdown,
+#     robot_startup,
+# )
 
-from eve.constants import DT, FOLLOWER_GRIPPER_JOINT_OPEN, START_ARM_POSE
+# from eve.constants import DT, FOLLOWER_GRIPPER_JOINT_OPEN, START_ARM_POSE
 
 from egomimic.utils.egomimicUtils import *
 
-from eve.robot_utils import move_grippers
-from eve.real_env import make_real_env
+# from eve.robot_utils import move_grippers
+# from eve.real_env import make_real_env
+
+from scipy.spatial.transform import Rotation as R
+
+extrinsics = EXTRINSICS['ariaJun7']
 
 joint_type_dict = {
     p.JOINT_REVOLUTE: "Revolute",
@@ -164,6 +168,105 @@ def rollout_ee_pose_real(robot_id, env, left_ee_pose, right_ee_pose, joints=[0, 
 
     return target_qpos_list
 
+def rollout_ee_pose_offline(robot_id, env, left_ee_pose, right_ee_pose, qpos):
+    target_qpos_list = []
+    qpos_list = []
+    for t, (left_pose, right_pose) in enumerate(zip(left_ee_pose, right_ee_pose)):
+
+        qpos = np.array(qpos[t])
+
+        qpos_left = qpos[:6]
+        qpos_right = qpos[7:13] # waist                  shoulder             elbow                   forearm_roll        wrist angle      wrist rotate 
+
+        qpos_left = qpos_left.tolist() + [0, 0.02239, -0.02239]
+        qpos_right = qpos_right.tolist() + [0, 0.02239, -0.02239] # ee_gripper   left finger     right finger
+        left_current_joint_positions = qpos_left
+        right_current_joint_positions = qpos_right
+        # print(f"CURRENT: {current_joint_positions}")
+
+        left_current_joint_positions[0] += math.pi/2
+        right_current_joint_positions[0] += math.pi/2
+
+        # target_pos = [pose[2], pose[0], pose[1]]
+        left_target_pos = left_pose[:3]
+        left_target_orientation = left_pose[3:]
+        t0 = time.time()
+        left_joint_angles = p.calculateInverseKinematics(
+            robot_id, 
+            endEffectorLinkIndex=12, 
+            targetPosition=left_target_pos, 
+            targetOrientation=left_target_orientation, 
+            currentPositions=left_current_joint_positions,
+            maxNumIterations=100,
+            residualThreshold=0.001
+        )
+        right_target_pos = right_pose[:3]
+        right_target_orientation = right_pose[3:]
+        right_joint_angles = p.calculateInverseKinematics(
+            robot_id, 
+            endEffectorLinkIndex=12, 
+            targetPosition=right_target_pos, 
+            targetOrientation=right_target_orientation, 
+            currentPositions=right_current_joint_positions,
+            maxNumIterations=100,
+            residualThreshold=0.001
+        )
+        t1 = time.time() - t0
+        print(f"Time per calcuation: {t1}")
+        left_joint_angles = list(left_joint_angles)
+        right_joint_angles = list(right_joint_angles)
+
+        left_target_qpos = np.array(left_joint_angles[:7])
+        right_target_qpos = np.array(right_joint_angles[:7])
+        left_target_qpos[0] -= math.pi/2
+        right_target_qpos[0] -= math.pi/2
+        # print(f"TARGET: {target_qpos}")
+
+        target_qpos = np.concatenate([left_target_qpos, right_target_qpos])
+
+        # ts = env.step(target_qpos)
+
+        qpos_list.append(qpos)
+        target_qpos_list.append(target_qpos)
+
+        # time.sleep(0.02)
+
+
+    return target_qpos_list
+
+
+def ee_pose_to_base_frame(ee_pose_cam, T_cam_base):
+    """
+    ee_pose_cam: (N, 3)
+    T_cam_base: (4, 4)  # transform from *camera to base* (same as in your code)
+    returns ee_pose_base: (N, 3)
+    """
+    N = ee_pose_cam.shape[0]
+    ee_pose_cam_h = np.concatenate([ee_pose_cam, np.ones((N, 1))], axis=1)  # (N,4)
+    ee_pose_base_h = (T_cam_base @ ee_pose_cam_h.T).T                       # (N,4)
+    return ee_pose_base_h[:, :3]
+
+
+def cam_cartesian_to_base_quat(cartesians_6dof, T_cam_base, euler_order='zyx', quat_format='xyzw'):
+    T = cartesians_6dof.shape[0]
+    pos_cam = cartesians_6dof[:, :3]                   # (T,3)
+    ang_cam = cartesians_6dof[:, 3:6]                   # (T,3)
+
+    pos_base = ee_pose_to_base_frame(pos_cam, T_cam_base)
+    
+    breakpoint()
+    R_cam_batch = R.from_euler(euler_order, ang_cam, degrees=False).as_matrix()   # (T,3,3)
+    R_base_cam  = T_cam_base[..., :3, :3]                                              # (3,3)
+    R_base_batch = np.einsum('ij,tjk->tik', R_base_cam, R_cam_batch)   
+    
+    quat_xyzw = R.from_matrix(R_base_batch).as_quat()                             # (T,4) x,y,z,w
+    if quat_format == 'wxyz':
+        quat_wxyz = np.concatenate([quat_xyzw[:, 3:4], quat_xyzw[:, :3]], axis=1)
+        quat_out = quat_wxyz
+    else:  # 'xyzw'
+        quat_out = quat_xyzw
+
+    return np.concatenate([pos_base, quat_out], axis=1)
 
 def main(args):
     
@@ -178,12 +281,12 @@ def main(args):
 
     aloha_fk = AlohaFK()
     
-    node = create_interbotix_global_node('aloha')
-    env = make_real_env(node, active_arms="both", setup_robots=True)
-    robot_startup(node)
+    # node = create_interbotix_global_node('aloha')
+    # env = make_real_env(node, active_arms="both", setup_robots=True)
+    # robot_startup(node)
 
     if args.debug:
-        episodes = [0]
+        episodes = [0, 1]
         dataset = RLDBDataset(repo_id=repo_id, root=root, local_files_only=True, episodes=episodes, mode="sample")
         joint_positions = torch.stack([
             sample["actions_joints"][0] for sample in dataset
@@ -191,16 +294,24 @@ def main(args):
         cartesians = torch.stack([
             sample["actions_cartesian"][0] for sample in dataset
         ])
+        
+        
         left_out = aloha_fk.fk(joint_positions[..., :6]).numpy()
         right_out = aloha_fk.fk(joint_positions[..., 7:13]).numpy()
-        right_fk_ee_pose = np.zeros((right_out.shape[0], 7))
-        left_fk_ee_pose = np.zeros((left_out.shape[0], 7))
-        for i, r in enumerate(right_out):
-            right_fk_ee_pose[i] = transformation_matrix_to_pose(r)
-        for i, l in enumerate(left_out):
-            left_fk_ee_pose[i] = transformation_matrix_to_pose(l)
+        # right_fk_ee_pose = np.zeros((right_out.shape[0], 7))
+        # left_fk_ee_pose = np.zeros((left_out.shape[0], 7))
+        # for i, r in enumerate(right_out):
+        #     right_fk_ee_pose[i] = transformation_matrix_to_pose(r)
+        # for i, l in enumerate(left_out):
+        #     left_fk_ee_pose[i] = transformation_matrix_to_pose(l)
+        
+        cartesians_left = cartesians[..., :6]
+        cartesians_right = cartesians[..., 7:]
+        
+        left_fk_ee_pose = cam_cartesian_to_base_quat(cartesians_left, extrinsics['left'])
+        right_fk_ee_pose = cam_cartesian_to_base_quat(cartesians_right, extrinsics['right'])
 
-        joint_positions_reconstructed = rollout_ee_pose_real(robot_id, env, left_fk_ee_pose, right_fk_ee_pose)
+        joint_positions_reconstructed = rollout_ee_pose_offline(robot_id, left_fk_ee_pose, right_fk_ee_pose)
         ###
         breakpoint()
         joint_positions_reconstructed = np.stack(joint_positions_reconstructed).astype(np.float32)
@@ -220,50 +331,6 @@ def main(args):
 
     p.disconnect()
 
-# def cartesian_joints(actions_cartesians):
-#     actions_gripper = actions_cartesian[..., -1].unsqueeze(-1)
-#     actions_pos = actions_cartesian[..., :3]
-#     actions_ypr = actions_cartesian[..., 3:6]
-#     actions_ypr = actions_ypr[..., list(permutation)]
-#     actions_rotmat = batched_euler_to_rot_matrix(actions_ypr)
-#     extrinsics = torch.from_numpy(self.model.model.camera_transforms.extrinsics[arm]).to(actions_cartesian.device).float()
-#     batch_shape = actions_cartesian.shape[:-1]
-#     T = torch.zeros(*batch_shape, 4, 4, device=actions_cartesian.device)
-#     T[..., :3, :3] = actions_rotmat
-#     T[..., :3, 3] = actions_pos
-#     T[..., 3, 3] = 1.0
-
-#     T_base = T
-#     T_base = extrinsics @ T # Should we multiply from base frame to cam frame?
-    
-#     # actions_rotmat = T_base[..., :3, :3]
-#     # wxyz -> xyzw
-
-#     # actions_quat = matrix_to_quaternion(actions_rotmat)
-#     # actions_quat = torch.cat([actions_quat[..., 1:], actions_quat[..., :1]], dim=-1)
-
-#     if T_base.dim() == 3:
-#         T_base = T_base.unsqueeze(1) # add the T dim
-#     if actions_gripper.dim() == 2:
-#         actions_gripper = actions_gripper.unsqueeze(0)
-#     actions = []
-#     for b in range(T_base.shape[0]):
-#         actions.append(transformation_matrix_to_pose(T_base[b].cpu().numpy()))
-
-#     actions = np.stack(actions)
-
-#     actions_pos = actions[..., :3]
-#     actions_quat = actions[..., 3:]
-#     target_joint_positions_l = []
-#     for i in range(batch_shape[0]):
-#         target_joint_positions = self.ik.solve(target_pos=actions_pos[i], 
-#                                             target_orientation=actions_quat[i], 
-#                                             current_joints=current_joint_positions)
-#         target_joint_positions_l.append(target_joint_positions)
-#     actions_joint_positions = np.stack(target_joint_positions_l)
-#     actions_joint_positions = np.concatenate((actions_joint_positions, actions_gripper.cpu().numpy()), axis=-1)
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
@@ -279,11 +346,11 @@ if __name__ == "__main__":
         help="path to dataset",
     )
 
-    parser.add_argument(
-        "--model",
-        type=str,
-        help="path to robot model"
-    )
+    # parser.add_argument(
+    #     "--model",
+    #     type=str,
+    #     help="path to robot model"
+    # )
     
     args = parser.parse_args()
     main(args)
