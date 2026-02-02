70c57d8bd9d4f36e2e76884fec2612904610bf0d
JIAQI CHEN <jiaqchen@ethz.ch>
2026-02-02 22:34:24 +0100

    new lerobot path

--- eth_clariden/clariden.sh
+++ eth_clariden/clariden.sh
@@ -1,7 +1,7 @@
 # also add env
 source /capstor/store/cscs/swissai/a144/jiaqchen/egoverse/faive2lerobot/faive2lerobot/bin/activate
 export PYTHONPATH=/capstor/store/cscs/swissai/a144/jiaqchen/egoverse/EgoVerse:$PYTHONPATH
-export PYTHONPATH=/capstor/store/cscs/swissai/a144/jiaqchen/data/EGOMIM/faive2lerobot/src/lerobot:$PYTHONPATH
+export PYTHONPATH=/capstor/store/cscs/swissai/a144/jiaqchen/egoverse/EgoVerse/external/lerobot:$PYTHONPATH
 
 export HF_HOME=/iopsstor/scratch/cscs/jiaqchen/.hf_cache
 export HF_DATASETS_CACHE=$HF_HOME/hf_datasets_cache
